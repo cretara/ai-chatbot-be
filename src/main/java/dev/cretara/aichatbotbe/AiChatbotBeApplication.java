1d3d6746dd62c726bd3b057d161a7a21a1016fbe
cretara <cretaradev@gmail.com>
2026-08-04 22:45:15 +0200

    Initial commit

---
+++ src/main/java/dev/cretara/aichatbotbe/AiChatbotBeApplication.java
@@ -0,0 +1,13 @@
+package dev.cretara.aichatbotbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatbotBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChatbotBeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dev/cretara/aichatbotbe/service/OpenAIService.java
@@ -0,0 +1,4 @@
+package dev.cretara.aichatbotbe.service;
+
+public interface OpenAIService {
+}
